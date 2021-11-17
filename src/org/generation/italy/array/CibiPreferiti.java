796a615424fda44e11e910a5a25dc4fd1ab2e855
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-17 15:22:52 +0100

    fine programma

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -3,7 +3,12 @@ package org.generation.italy.array;
 public class CibiPreferiti {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] cibiPreferiti = {"Pasta al forno" ,"Pizza", "Panino con Hamburger", "Sushi", "Cotoletta", "Scaloppine al limone", "Risotto allo zafferano"};
+		
+		System.out.println("La mia classifica contiene " + cibiPreferiti.length + " pietanze.");
+		System.out.println("Il mio piatto top è: " + cibiPreferiti[0]);
+		System.out.println("Il mio piatto preferito, ma non troppo è: " + cibiPreferiti[cibiPreferiti.length - 1]);
 
 	}
 
